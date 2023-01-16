2acef2609c2b66abeb2c229dc6130b8d09b67a41
milicad00 <draca.milica10@gmail.com>
2023-01-16 21:15:17 +0100

    Frontend app setup

---
+++ frontend/project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <LoginPage></LoginPage>
+  </div>
+</template>
+
+<script>
+
+import LoginPage from './components/LoginPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginPage
+  }
+}
+</script>
---
+++ frontend/project/src/components/LoginPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Please log in</h1>
+</template>
+
+<script>
+export default {
+    name: 'LoginPage'
+}
+</script>
